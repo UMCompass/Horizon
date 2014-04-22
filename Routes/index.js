64c04ccb25fa013578c573607d9e667babb2e0b7
jtyler92 <jtyler@umass.edu>
2014-04-22 19:13:32 -0400

    Everything I've been working on today
    It's not complete, but now you can all see what I've got.

--- Routes/index.js
+++ Routes/index.js
@@ -1,26 +1,62 @@
 /**
- * Created by John on 4/21/14.
+ * Adapted from workshop 5
  */
 
-
-exports.authorize = function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    if (!username || !password) {
-        req.flash('authorize', 'Must provide username and password');
-        res.redirect('/');
+/*pseudo code implementation of changeUsername*/
+exports.changeUsername = function(req, res) {
+    var newUserName = req.body.username;
+    if (newUserName !== ""){
+        if (database.changeUserName(userID, newUserName)) {
+            req.session.user.username = newUserName;
+            req.flash("changeUsername", "Username successfully changed to: " + newUserName)
+        }
+        else {
+            req.flash("changeUsername", "There seems to be a server error, please try again.")
+        }
     }
     else {
-        res.redirect('../checklist')
-        /*users.validate(username, password, function (err, user) {
-            if (err) {
-                req.flash('authorize', err);
-                res.redirect('/');
+        req.flash("changeUsername", "Oh my god, fill in the fucking box. There's only one, it's not that hard, idiot.")
+    }
+}
+
+/*pseudo code implementation of changePassword*/
+exports.changePassword = function(req, res) {
+    var newPassword1 = req.body.password1;
+    var newPassword2 = req.body.password2;
+    if (newPassword1 !== "" && newPassword2 !== ""){
+        if (newPassword1 === newPassword2){
+            if (database.changeUserName(userID, newPassword1)) {
+                req.session.user.password = newPassword1;
             }
             else {
-                req.session.user = user;
-                res.redirect('../checklist');
+                req.flash("changePassword", "There seems to be a server error, please try again.")
             }
-        });*/
+        }
+        else {
+            req.flash("changePassword", "You're a fucking idiot, learn to type a God damned password." +
+                "Try again and make sure they match this time, asshole.")
+        }
     }
-};
+    else {
+        req.flash("changePassword", "First day on the internet? Hey Shitforbrains, " +
+            "You have to fill in both text boxes. Please try again.")
+    }
+}
+
+/*pseudo code implementation of changeEmail*/
+exports.changeEmail = function(req, res) {
+    var newEmail = req.body.email;
+    if (newEmail !== ""){
+        if (database.changeUserName(userID, newEmail)) {
+            req.session.user.email = newEmail;
+            req.flash("changeEmail", "Email successfully changed to: " + newEmail)
+        }
+        else {
+            req.flash("changeEmail", "There seems to be a server error, please try again.")
+        }
+    }
+    else {
+        req.flash("changeEmail", "Holy shit, all you have to do is fill in that one box." +
+            "You're a fucking idiot.")
+    }
+}
--- SettingsPage/Settings.js
+++ SettingsPage/Settings.js
@@ -5,20 +5,23 @@ function displaySettingsItem(settingsItem){
 
     var form = document.createElement('form');
     if (settingsItem.id === "username") {
+        form.setAttribute('action', 'changeUsername')
         form.innerHTML = "" +
             '<label for="newUsername">New Username:</label><br>' +
             '<input id="newUsername" type="text" name="username"><br>' +
             '<input id="submit" type="submit" value="Change Username">'
     }
     else if (settingsItem.id === "password") {
+        form.setAttribute('action', 'changePassword')
         form.innerHTML = "" +
             '<label for="newPassword">New Password:</label><br>' +
-            '<input id="newPassword" type="password" name="password"><br>' +
+            '<input id="newPassword" type="password" name="password1"><br>' +
             '<label for="newPassword2">Re-Enter New Password:</label><br>' +
-            '<input id="newPassword2" type="password" name="password"><br>' +
+            '<input id="newPassword2" type="password" name="password2"><br>' +
             '<input id="submit" type="submit" value="Change Password">'
     }
     else if (settingsItem.id === "email") {
+        form.setAttribute('action', 'changeEmail')
         form.innerHTML = "" +
             '<label for="newEmail">Email:</label><br>' +
             '<input id="newEmail" type="text" name="email"><br>' +
