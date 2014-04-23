5fb20206e0a0aa4d9968db7d1e40c8e3f3f84184
jtyler92 <jtyler@umass.edu>
2014-04-22 20:03:08 -0400

    Removed app.js

--- Routes/index.js
+++ Routes/index.js
@@ -8,15 +8,16 @@ exports.changeUsername = function(req, res) {
     if (newUserName !== ""){
         if (database.changeUserName(userID, newUserName)) {
             req.session.user.username = newUserName;
-            req.flash("changeUsername", "Username successfully changed to: " + newUserName)
+            req.flash("changeUsername", "Username successfully changed to: " + newUserName);
         }
         else {
-            req.flash("changeUsername", "There seems to be a server error, please try again.")
+            req.flash("changeUsername", "There seems to be a server error, please try again.");
         }
     }
     else {
-        req.flash("changeUsername", "Oh my god, fill in the fucking box. There's only one, it's not that hard, idiot.")
+        req.flash("changeUsername", "Oh my god, fill in the fucking box. There's only one, it's not that hard, idiot.");
     }
+    res.redirect("/");
 }
 
 /*pseudo code implementation of changePassword*/
@@ -27,20 +28,22 @@ exports.changePassword = function(req, res) {
         if (newPassword1 === newPassword2){
             if (database.changeUserName(userID, newPassword1)) {
                 req.session.user.password = newPassword1;
+                req.flash("changePassword", "Password successfully changed");
             }
             else {
-                req.flash("changePassword", "There seems to be a server error, please try again.")
+                req.flash("changePassword", "There seems to be a server error, please try again.");
             }
         }
         else {
             req.flash("changePassword", "You're a fucking idiot, learn to type a God damned password." +
-                "Try again and make sure they match this time, asshole.")
+                "Try again and make sure they match this time, asshole.");
         }
     }
     else {
         req.flash("changePassword", "First day on the internet? Hey Shitforbrains, " +
-            "You have to fill in both text boxes. Please try again.")
+            "You have to fill in both text boxes. Please try again.");
     }
+    res.redirect("/");
 }
 
 /*pseudo code implementation of changeEmail*/
@@ -49,14 +52,15 @@ exports.changeEmail = function(req, res) {
     if (newEmail !== ""){
         if (database.changeUserName(userID, newEmail)) {
             req.session.user.email = newEmail;
-            req.flash("changeEmail", "Email successfully changed to: " + newEmail)
+            req.flash("changeEmail", "Email successfully changed to: " + newEmail);
         }
         else {
-            req.flash("changeEmail", "There seems to be a server error, please try again.")
+            req.flash("changeEmail", "There seems to be a server error, please try again.");
         }
     }
     else {
         req.flash("changeEmail", "Holy shit, all you have to do is fill in that one box." +
-            "You're a fucking idiot.")
+            "You're a fucking idiot.");
     }
+    res.redirect("/");
 }
