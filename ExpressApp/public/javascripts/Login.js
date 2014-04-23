85b62506ebefb876255bca5d06b442ad2da58ef5
haiqnguyen318 <hai@umass.edu>
2014-04-22 20:06:58 -0400

    Express App
    Login, register, showing mock checklist working

---
+++ ExpressApp/public/javascripts/Login.js
@@ -0,0 +1,43 @@
+/**
+ * Created by John on 4/12/14.
+ */
+
+database = {
+    "School_Name": "UMass"
+};
+
+/*function initializeSideBar(database) {
+    var sidebar = document.getElementById("sidebar");
+    console.log(sidebar);
+    var compass = document.createElement("p");
+    compass.setAttribute("id", "compass");
+    compass.innerHTML = "The " + database.School_Name + " Compass";
+    console.log(compass);
+    var horizon = document.createElement("p");
+    horizon.setAttribute("id", "horizon");
+    horizon.innerHTML = "by Horizon";
+    sidebar.appendChild(compass);
+    sidebar.appendChild(horizon);
+}*/
+
+$(document).ready(function(){
+    /*initializeSideBar(database);*/
+
+    $("#register").click(function(){
+        var center = document.getElementById("center");
+        center.innerHTML = '' +
+            '<div id="title">New User Registration</div>' +
+            '<form method="post" action="register">' +
+                '<label for="newUsername">Username:</label><br>' +
+                '<input id="newUsername" type="text" name="username"><br>' +
+                '<label for="newPassword">Password:</label><br>' +
+                '<input id="newPassword" type="password" name="password"><br>' +
+                '<label for="newPassword2">Re-Enter Password:</label><br>' +
+                '<input id="newPassword2" type="password" name="password1"><br>' +
+                '<label for="newEmail">Email:</label><br>' +
+                '<input id="newEmail" type="text" name="email"><br>' +
+                '<input id="submit" type="submit" value="Create">' +
+            '</form>';
+    });
+
+});
---
+++ ExpressApp/routes/index.js
@@ -0,0 +1,100 @@
+
+//Freddy Nguyen
+
+var MongoClient = require('mongodb').MongoClient;
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.login = function(req,res){
+	var user = req.session.user;
+	if(user){
+		res.redirect('checklist');
+	}
+	else{
+		res.render('login');
+	}
+};
+
+exports.authorize = function(req,res){
+	var username = req.body.username;
+	var password = req.body.password;
+
+	if(!username || !password){
+		req.flash('authorize','You must provide username and password');
+		res.redirect('/');
+	}
+
+	else{
+		MongoClient.connect('mongodb://localhost:27017',function(err,db){
+      		var users = db.collection('users');
+      		users.findOne({username: username, password: password},function(err, item){
+        		if(item===null){
+          			req.flash('authorize','Email or Password Invalid');
+          			res.redirect('/');
+        		}
+        		else{
+          			req.session.user = item;
+          			users.update({user: username},{$set: {online: true}},function(err,result){
+            			if (err) throw err;
+            			console.log(result);
+          			});
+          			res.redirect('/checklist');
+        		}
+      		});
+    	});
+	}
+};
+
+exports.register = function(req,res){
+	var user = req.body.username;
+	var pass = req.body.password;
+	var repass = req.body.password1;
+	var email = req.body.email;
+	if(!(user && pass && repass && email) || pass!=repass){
+		res.redirect('/');
+	}
+
+	else{
+		MongoClient.connect('mongodb://localhost:27017',function(err,db){
+			var users = db.collection('users');
+			users.findOne({username:user},function(err,item){
+				if (item===null){
+					var newUser = {username: user,password:pass,email:email};
+					users.insert(newUser,function(err,result){
+						if (err) throw err;
+						console.log(result);
+					});
+					req.session.user = newUser;
+					res.render('checklist');
+				}
+
+				else{
+					res.redirect('/');
+				}
+			});
+		});
+	}
+};
+
+exports.checklist = function(req,res){
+	var user = req.session.user;
+	if(!user){
+		res.redirect('/');
+	}
+	
+	else{
+		res.render('checklist');
+	}
+};
+
+exports.settings = function(req,res){
+	var user = req.session.user;
+	if (!user){
+		res.redirect('/');
+	}
+	else{
+		res.render('settings');
+	}
+};
