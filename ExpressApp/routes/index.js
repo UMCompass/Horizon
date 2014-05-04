71887900da1694dda8e2960be99bf2ef0ae3a9d3
xxlillusionlxx <jdale@umass.edu>
2014-05-04 03:46:39 -0400

    Some stuff
    Split the retrieve route up

--- ExpressApp/routes/index.js
+++ ExpressApp/routes/index.js
@@ -270,18 +270,12 @@ exports.retrieve = function(req, res) {
 		res.redirect('/');
 	}
 	else {
-		/*chlist.getList(function(list) {
-			res.send(list);
-		}); */ 
 		MongoClient.connect('mongodb://localhost:27017',function(err,db){
 			var collection = db.collection('checklist');
 			goodies = collection.find().toArray(function(err,result){
 						if (err) throw err;
-						var x = JSON.stringify(result);
-						/*console.log(result[0].name);*/
 						res.send(result);
 			});
-			//res.send(goodies);
 		});
 	}
 }
